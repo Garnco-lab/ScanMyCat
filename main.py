0804b9d36f72c63251cd4f7fc21551abf3995c96
Steven <stevendeleonwork@gmaill.com>
2022-02-28 00:28:22 -0800

    added scan button and working on basic animations

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from sklearn.preprocessing import LabelEncoder
 import catScanner
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.camera import Camera
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
+from kivy.animation import Animation
+from kivy.clock import Clock
+from kivy.core.audio import SoundLoader
+
+class ImageButton(ButtonBehavior, Image):
+    pass
 
 
 class SayHello(App):
@@ -17,14 +24,15 @@ class SayHello(App):
         self.breed_dictionary = list(df_labels["breed"].value_counts().keys())
         self.camera_obj = Camera()
         self.camera_obj.resolution = (800, 800)
+        self.countdown = 0
 
         self.cat_breed_name = "Nothing"
 
-        self.image = Image(source="ui_pictures/catfunnyimage2.png")
+        self.image = Image(source="ui_pictures/catfunnyimage.png", width=500, allow_stretch=True)
 
-        button_obj = Button(text="Click Here")
+        button_obj = ImageButton(source="ui_pictures/scancatbutton.png")
         button_obj.size_hint = (0.5, 0.2)
-        button_obj.pos_hint = {"x": 0.25, "y": 0.25}
+        button_obj.pos_hint = {"x": 0.25, "y": 0.05}
         button_obj.bind(on_press=self.take_selfie)
 
         self.exit_button = Button(text="Exit")
@@ -33,9 +41,14 @@ class SayHello(App):
 
         layout = FloatLayout()
         layout.add_widget(self.camera_obj)
-        layout.add_widget(button_obj)
         layout.add_widget(self.greeting)
         layout.add_widget(self.image)
+        layout.add_widget(button_obj)
+        Clock.schedule_interval(self.catChange, 1.)
+        # self.catImage = Image(source="images/selfie.jpg", opacity=0)
+        # layout.add_widget(self.catImage)
+        # self.anim = Animation(opacity=0.5, x=0.5)
+        # self.anim.start(self.catImage)
 
         return layout
 
@@ -47,6 +60,11 @@ class SayHello(App):
         self.cat_breed_name = catScan.scan_cat()
         self.greeting.text = self.cat_breed_name
 
+    def catChange(self, *args):
+        self.countdown += 1
+        print(self.countdown)
+        if self.countdown > 7:
+            self.image.source = "ui_pictures/catfunnyimage2.png"
 
 if __name__ == "__main__":
     SayHello().run()
